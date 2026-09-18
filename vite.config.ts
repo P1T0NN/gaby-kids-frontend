f39b340d3ec504ce30da06193be7d477502bbf2d
P1T0NN <ognjen.tapuskovic@gmail.com>
2026-09-18 15:12:02 +0200

    Changed to adapter vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -43,9 +43,7 @@ export default defineConfig({
 				remoteFunctions: true
 			},
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+			// adapter-vercel targets Vercel's build output and runtime, see https://svelte.dev/docs/kit/adapter-vercel.
 			adapter: adapter()
 		})
 	],
